8de7380134cd23fd3550f5639eb476fb87a8d522
Matthew Adebayo <olumatty@gmail.com>
2024-10-08 22:35:12 +0100

    first commit

---
+++ components/Feed.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import PromptCrad from "./PromptCrad";
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (e) => {};
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Feed;
---
+++ components/PromptCrad.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PromptCrad = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PromptCrad
